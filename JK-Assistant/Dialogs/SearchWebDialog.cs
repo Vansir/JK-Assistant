60de234acedbd2f357f194c2a1938b8a6d94137b
Jakub Koster <jakub.koster@symphonyhq.com>
2021-08-21 22:08:11 +0200

    Added search result classes and corrected the method of search request and result display

--- JK-Assistant/Dialogs/SearchWebDialog.cs
+++ JK-Assistant/Dialogs/SearchWebDialog.cs
@@ -1,67 +1,100 @@
 ﻿using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.LanguageGeneration;
 using Microsoft.Bot.Schema;
 using Newtonsoft.Json.Linq;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web;
+using System.Text.Json;
+using System;
+using Microsoft.Extensions.Configuration;
 
 namespace JK_Assistant
 {
     public class SearchWebDialog : ComponentDialog
     {
-        private const string _webSearchCardPrompt = "WebSearchCard";
-        private const string _googleSearchUrlPrefix = "https://www.google.com/search?q=";
+        private const string _webSearchMessage = "WebSearchCard";
+        private const string _googleCustomSearchUri = "https://www.googleapis.com/customsearch/v1";
+        private const string _googleSearchUrl = "https://www.google.com/search?q=";
+        private readonly IConfiguration _config;
+
+        private static readonly HttpClient client = new HttpClient();
+        private Templates _templates;
         public SearchWebDialog()
         {
             InitialDialogId = nameof(SearchWebDialog);
 
-            AddDialog(new TextPrompt(_webSearchCardPrompt,WebSearchCardValidatorAsync));
+            string[] paths = { ".", "Resources", "LanguageGeneration.lg" };
+            string fullPath = Path.Combine(paths);
+            _templates = Templates.ParseFile(fullPath);
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
 
             //main waterfall dialog
             AddDialog(new WaterfallDialog(nameof(SearchWebDialog), new WaterfallStep[]
             {
-                SearchCardStepAsync,
+                SearchInputStepAsync,
                 DisplayWebpage,
             }));
         }
 
-        private async Task<DialogTurnResult> SearchCardStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> SearchInputStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             //Create Adaptive Card with web search
             Attachment webSearchCardAttachment = Functions.CreateWebSearchCardAttachment();
 
             //Return prompt with adaptive card
-            return await stepContext.PromptAsync(_webSearchCardPrompt,
+            return await stepContext.PromptAsync(nameof(TextPrompt),
                 new PromptOptions
                 {
-                    Prompt = (Activity)MessageFactory.Attachment(webSearchCardAttachment),
+                    Prompt = ActivityFactory.FromObject(_templates.Evaluate("InputSearch")),
                 }, cancellationToken);
         }
 
         private async Task<DialogTurnResult> DisplayWebpage(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            //Get user choice.
-            JObject jObject = stepContext.Context.Activity.Value as JObject ?? null;
+            //Store value to search for
+            string searchValue = (string)stepContext.Result;
 
-            if (!(jObject is null))
-            {
-                //Store value to search for
-                string searchValue = (string)jObject["SearchValue"];
+            //Build url to use
+            string searchUrl = _googleSearchUrl + searchValue;
 
-                //Build url to use
-                string searchUrl = _googleSearchUrlPrefix + searchValue;
+            var searchResultCard = MessageFactory.Attachment(Functions.CreateSearchResultCardAttachment(searchUrl));
 
-                var searchResultCard = MessageFactory.Attachment(Functions.CreateSearchResultCardAttachment(searchUrl));
-
-                await stepContext.Context.SendActivityAsync(searchResultCard);
-            }
-
+            await stepContext.Context.SendActivityAsync(searchResultCard);
+        
             return await stepContext.EndDialogAsync(null, cancellationToken);
         }
 
         private static Task<bool> WebSearchCardValidatorAsync(PromptValidatorContext<string> promptValidatorContext, CancellationToken cancellationToken)
         {
             return Task.FromResult(true);
+        }
+
+        private static async Task<SearchResultRoot> GetSearchResults(string searchValue)
+        {
+            var builder = new UriBuilder(_googleCustomSearchUri);
+            builder.Port = -1;
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query["key"] = apiKey;
+            query["cx"] = searchEngineID;
+            query["num"] = searchResultsNumber;
+            query["q"] = searchValue;
+
+            builder.Query = query.ToString();
+
+            var check = builder.ToString();
+
+            var streamTask = client.GetStreamAsync(builder.ToString());
+            var searchResults = await JsonSerializer.DeserializeAsync<SearchResultRoot>(await streamTask);
+
+            foreach (var item in searchResults.Items)
+            {
+                Console.WriteLine(item.title);
+            }
         }
     }
 }
---
+++ JK-Assistant/Models/SearchResultRoot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace JK_Assistant
+{
+    class SearchResultRoot
+    {
+        [JsonPropertyName("items")]
+        public List<SearchResultItem> Items { get; set; }
+    }
+}
